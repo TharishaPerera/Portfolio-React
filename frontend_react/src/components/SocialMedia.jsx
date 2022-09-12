e1e02252a8bebb946770a7ae7da7f7f1e65fdb05
Baxbie <tharishaperera@gmail.com>
2022-09-12 22:59:05 +0530

    add social icons

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BsInstagram } from 'react-icons/bs';
-import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+import { BsInstagram, BsGithub } from 'react-icons/bs';
+import { FaFacebookF, FaLinkedinIn, FaBlogger } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
@@ -8,6 +8,18 @@ const SocialMedia = () => {
       <div>
         <a href="https://www.linkedin.com/in/tharisha-perera" target='_blank' rel='noreferre'>
           <FaLinkedinIn />
+        </a>
+      </div>
+
+      <div>
+        <a href="https://github.com/TharishaPerera/" target='_blank' rel='noreferre'>
+          <BsGithub />
+        </a>
+      </div>
+
+      <div>
+        <a href="https://blog-tharishaperera.netlify.app/" target='_blank' rel='noreferre'>
+          <FaBlogger />
         </a>
       </div>
 
